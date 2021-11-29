a24178f516eaa6275c89208c5750095f92579028
shegoestech202122 <artjoms.v.petrovs@gmail.com>
2021-11-29 02:02:07 +0200

    Initial commit

---
+++ app/src/main/java/lv/artjoms/spaceshipmanagementsystem/MainActivity.java
@@ -0,0 +1,47 @@
+package lv.artjoms.spaceshipmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edit1, edit2;
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edit1 = findViewById(R.id.username);
+        edit2 = findViewById(R.id.password);
+        btn1 = findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // LOGIN
+                login();
+            }
+        });
+    }
+
+    public void login() {
+        String username = edit1.getText().toString();
+        String password = edit2.getText().toString();
+
+        // Validation of the username and password
+        if( username.equals("") || password.equals("")) {
+            Toast.makeText(this, "Username or password are empty", Toast.LENGTH_LONG).show();
+        } else if ( username.equals("Artjoms") && password.equals("qwerty")) {
+            Toast.makeText(this, "login was succesful", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Username or Password is incorrect", Toast.LENGTH_LONG).show();
+        }
+    }
+}
